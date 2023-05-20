713ebe146d1ec89e8dde237ffc2258678b60a2de
nishchal <singhnishchal27@gmail.com>
2023-05-20 23:24:26 +0530

    posts API completed

--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
 
 const app = express();
-const post = {};
+app.use(bodyParser.json());
+const posts = {};
 
 app.get('/posts', (req,res)=>{
-
+    res.send(posts);
 });
 
 app.post('/posts', (req,res)=>{
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
 
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
 });
 
 app.listen(4000, ()=>{
